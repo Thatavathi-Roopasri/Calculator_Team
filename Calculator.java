75f4dfa7d6125b1dc2214fbd69f1f74e00650b60
Thatavathi-Roopasri <Thatavathi-Roopasri@users.noreply.github.com>
2025-12-23 13:54:52 +0530

    Add Calculator.java with addition method

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    
+    // Method to add two numbers
+    public double add(double a, double b) {
+        return a + b;
+    }
+    
+    // Main method to test the calculator
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        double num1 = 10;
+        double num2 = 5;
+        
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + calc.add(num1, num2));
+    }
+}
